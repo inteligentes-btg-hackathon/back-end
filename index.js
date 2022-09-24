37874e875eab3afff0a7ee875a2d8174caaf6f44
ViniciosLugli <vinicioslugli@gmail.com>
2022-09-24 11:31:41 -0300

    Configurate sequelize-cli env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
+const database = require('./database');
 
 dotenv.config();
 
