bc539a8b15892745b9a0ca8055fc998f39e162da
ViniciosLugli <vinicioslugli@gmail.com>
2022-09-24 19:38:55 -0300

    Remake project

--- controllers/darf.controller.js
+++
@@ -1,3 +0,0 @@
-class DarfController {}
-
-module.exports = DarfController();
---
+++ src/models/client.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+# https://www.w3schools.com/python/python_datatypes.asp
+# https://fastapi.tiangolo.com/tutorial/response-model/#__tabbed_1_3
+
+
+class Client(BaseModel):
+    customer_id: str
